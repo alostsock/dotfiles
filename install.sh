4c8edcf508da33c0465a972557d276a09414515b
alostsock <49344439+alostsock@users.noreply.github.com>
2021-06-07 01:06:55 -0400

    Add python build deps to install script

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 apt update
 apt -y upgrade
 
-apt install -y neovim stow tree fzf silversearcher-ag build-essential\
+PKGS="neovim stow tree fzf silversearcher-ag"
+
+PYTHON_BUILD_DEPS="make build-essential libssl-dev zlib1g-dev\
+ libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm\
+ libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev\
+ libffi-dev liblzma-dev"
+
+apt install -y $PKGS $PYTHON_BUILD_DEPS
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
--- shell/.bashrc
+++ shell/.bashrc
@@ -122,8 +122,8 @@ export NVM_DIR="$HOME/.nvm"
 
 # pyenv
 if [ -d "$HOME/.pyenv/bin" ]; then
-  export PATH="$HOME/.pyenv/bin:$PATH"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
 fi
 
