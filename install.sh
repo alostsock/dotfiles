ea8c0c66be5c695ac9feecc5675f1fd9f608c558
alostsock <49344439+alostsock@users.noreply.github.com>
2022-02-26 22:05:05 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt update
 sudo apt -y upgrade
 
 PKGS="neovim stow tree fzf silversearcher-ag"
 
-PYTHON_BUILD_DEPS="make build-essential libssl-dev zlib1g-dev\
- libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm\
- libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev\
- libffi-dev liblzma-dev"
+PYTHON_BUILD_DEPS="make build-essential libssl-dev zlib1g-dev libbz2-dev \
+  libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils \
+  tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
 
 sudo apt install -y $PKGS $PYTHON_BUILD_DEPS
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+LANG_SERVERS="vscode-langservers-extracted typescript \
+  typescript-language-server"
+npm install -g $LANG_SERVERS
 
 # pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
